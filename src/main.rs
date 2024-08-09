fcc5d41b1416d6d52c12aee50aee733c8be3f8af
Ryan Ruckley <rruckley@gmail.com>
2024-08-09 10:18:28 +1000

    Updates

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,6 @@ struct CodeResponse {
     hash: String,
 }
 
-/// This is the main body for the function.
-/// Write your code inside it.
-/// There are some code example in the following URLs:
-/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
 async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
     // Extract some useful information from the request
     match event.body() {
